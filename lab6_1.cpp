83a60b86f59aed7ad81a1dcb400cb32b73a3e15c
archiash <archiash.legacy@gmail.com>
2023-12-01 13:33:23 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int odd = 0;
+    int even = 0;
+    int current = -1;
+    while(current != 0){
+        cout << "Enter an integer: ";
+        cin >> current;
+        if(current != 0){
+        if(current % 2 == 0)  even++;
+        else odd++;}
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
